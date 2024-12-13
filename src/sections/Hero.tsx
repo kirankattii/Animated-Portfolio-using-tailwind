ed4a5562cd5f850a0bf78b877067889f9e1feba6
kiran katti <kirankatti550@gmail.com>
2024-12-13 21:38:48 +0530

    done

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -140,7 +140,7 @@ export const HeroSection = () => {
 						Building Expectional User Experiences
 					</h1>
 					<p className="mt-4 text-center text-white/60 md:text-lg">
-				A full-stack web developer with 2+ years of experience, specializing in the MERN stack, ReactJS, NextJS, and scalable RESTful APIs.
+				A full-stack web developer with 1+ years of experience, specializing in the MERN stack, ReactJS, NextJS, and scalable RESTful APIs.
 					</p>
 				</div>
 				<div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
